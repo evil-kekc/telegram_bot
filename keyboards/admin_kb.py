21e29c6316b84824e3353177ae87a8419e8d8116
evil-kekc <82170725+evil-kekc@users.noreply.github.com>
2021-11-19 18:35:38 +0300

    Update admin_kb.py

--- keyboards/admin_kb.py
+++ keyboards/admin_kb.py
@@ -7,6 +7,7 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 button_load = KeyboardButton('Загрузить')
 button_cancel = KeyboardButton('Отмена')
 button_exit = KeyboardButton('Выход')
+button_client_inf = KeyboardButton('Информация о заказах')
 
 
 
@@ -16,6 +17,7 @@ button_case_admin = ReplyKeyboardMarkup(resize_keyboard=True).add(button_load).a
 
 button_cancel_admin = ReplyKeyboardMarkup(resize_keyboard=True).add(button_cancel)
 
-button_exit_admin = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(button_load).add(button_exit)
+button_exit_admin = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(button_load)\
+    .add(button_client_inf).add(button_exit)
 
 button_choise_client = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add().add(button_exit)
