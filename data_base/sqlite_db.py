fd934834c3d91282de79151f0e5340966fb8c7fd
evil-kekc <82170725+evil-kekc@users.noreply.github.com>
2021-11-19 18:34:23 +0300

    Update sqlite_db.py

--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -14,13 +14,13 @@ def sql_start():
 
 
 def sql_start_1():
-    global base, cur
-    base = sq.connect('vsemcveti.db')
-    cur = base.cursor()
+   global base, cur
+    base = sq.connect('vsemcveti.db')  # Подключаемся к БД или создает БД (если ее нет)
+    cur = base.cursor()  # Создаем курсор
     if base:
         print('\nData base zakaz connected!')
     base.execute(
-        'CREATE TABLE IF NOT EXISTS zakaz(A TEXT PRIMARY KEY, B TEXT, C TEXT)')
+        'CREATE TABLE IF NOT EXISTS zakaz(id TEXT, name TEXT, numb TEXT, photo, telephone TEXT, dost TEXT)')
     base.commit()  # Сохранен
 
 
@@ -30,10 +30,9 @@ async def sql_add_admin_command(state):
         cur.execute('INSERT INTO album VALUES (?, ?, ?, ?)', tuple(data.values()))
         base.commit()
 
-async def sql_add_zakaz_command(state):
-    async with state.proxy() as data:  # Открываем словарь
-        cur.execute('INSERT INTO zakaz VALUES (?, ?, ?, ?, ?)', tuple(data.values()))
-        base.commit()
+async def sql_add_zakaz_command(ID, db_name, number, photo, telephone, dost):
+    cur.execute('INSERT INTO zakaz VALUES (?, ?, ?, ?, ?, ?)', (ID, db_name, number, photo, telephone, dost))
+    base.commit()
 
 """Читаем и отправляем сообщением базу данных"""
 
@@ -43,4 +42,12 @@ async def sql_read_admin(message):
 
 async def sql_read_zakaz(message):
     for ret in cur.execute('SELECT * FROM zakaz').fetchall():
-        await bot.send_photo(message.from_user.id, f'ID: {ret[0]}\n Имя: {ret[1]}\nФамилия: {ret[2]}\nТелефон: {ret[3]}\nЦена: {ret[-1]} BYN')
+        if ret[3] == None:
+            await bot.send_message(message.from_user.id,
+                                 f'<b>ID:</b> {ret[0]}\n<b>Имя:</b> {ret[1]}\n<b>Номер букета:</b> {ret[2]}\n<b>Телефон</b>: {ret[4]},'
+                                 f'\n<b>Доставка:</b> {ret[-1]}', parse_mode=types.ParseMode.HTML)
+        else:
+            await bot.send_photo(message.from_user.id, ret[3], f'<b>ID:</b> {ret[0]}\n<b>Имя:</b> {ret[1]}'
+                                                               f'\n<b>Номер букета:</b> Фото\n<b>Телефон:</b> {ret[4]},'
+                                                               f'\n<b>Доставка:</b> {ret[-1]}',
+                                 parse_mode=types.ParseMode.HTML)
