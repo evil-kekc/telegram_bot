04ef348bb8308a3fd08bc58d72c9004db19c8e4f
evil-kekc <82170725+evil-kekc@users.noreply.github.com>
2021-11-19 18:46:14 +0300

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -84,7 +84,6 @@ async def client_inf(message: types.Message, state: FSMContext):
     current_state = await state.get_state()
     if current_state is None:
         return
-    await state.finish()
     await sqlite_db.sql_read_zakaz(message)
     
         
